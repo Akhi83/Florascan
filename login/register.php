1b67479d82c32fce46797bebae393763f98b9be6
Anannya <105483863+AnnaJoshi26@users.noreply.github.com>
2024-09-26 15:40:57 +0530

    Create register.php
    Register.php

---
+++ login/register.php
@@ -0,0 +1,93 @@
+<?php 
+
+include 'config.php';
+
+error_reporting(0);
+
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+	$cpassword = md5($_POST['cpassword']);
+
+	if ($password == $cpassword) {
+		$sql = "SELECT * FROM users WHERE email='$email'";
+		$result = mysqli_query($conn, $sql);
+		if (!$result->num_rows > 0) {
+			$sql = "INSERT INTO users (username, email, password, status,image)
+					VALUES ('$username', '$email', '$password','logged in','profile.png')";
+			$result = mysqli_query($conn, $sql);
+			if ($result) {
+				echo '<script type="text/javascript">'; 
+				echo 'alert("Registration Completed.");'; 
+				echo 'window.location= "index.php";';
+				echo '</script>';   
+				// echo "<script>alert('Registration Completed.')</script>";
+				
+				// $username = "";
+				// $email = "";
+				// $_POST['password'] = "";
+				// $_POST['cpassword'] = "";
+				
+			} else {
+				echo "<script>alert('Something Wrong Went.')</script>";
+			}
+		} else {
+			echo "<script>alert('Email Already Exists.')</script>";
+		}
+		
+	} else {
+		echo "<script>alert('Passwords Do Not Match.')</script>";
+	}
+}
+// function myButton()
+// {
+// 	header("Location: index.php");
+// }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="index2.css">
+
+	<title>Register</title>
+	<link rel="icon" type="image/x-icon" href="../HomePage/favicon2.ico">
+</head>
+<body>
+	<div class="container">
+		<form action="" method="POST" class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Register Here.</p>
+			<div class="input-group">
+				<input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password" minlength="5" value="<?php echo $_POST['password']; ?>" required>
+            </div>
+            <div class="input-group">
+				<input type="password" placeholder="Confirm Password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+			</div>
+			<div class="input-group">
+				<button name="submit" class="btn">Register</button>
+				
+			</div>
+			<p class="login-register-text">Have an account? <a href="index.php">Login Here</a>.</p>
+		</form>
+	</div>
+</body>
+</html>
